431b9392a0099d6c91ed87d8e6265c002bfb3643
yuguangcan <yuguangcan@breadtrip.com>
2014-11-13 16:43:55 +0800

    footer

--- app/template/widget/footer.tpl
+++ app/template/widget/footer.tpl
@@ -4,14 +4,14 @@
         <li>
             <a href="/community/index/index">首页</a>
         </li>
-        <li>|</li>
+       <!--  <li>|</li>
         <li>
             <a href="/community/repair/listview">帮助</a>
         </li>
         <li>|</li>
         <li>
             <a href="/community/repair/listview">反馈</a>
-        </li>
+        </li> -->
     </ul>
     <div>&copy;{%date('Y')%}&nbsp;领结科技</div>
 </footer>
--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ domain="o2o@115.28.208.154"
 scp -r dist/static/*  ${domain}:/home/o2o/webroot/static/
 scp -r dist/template/*  ${domain}:/home/o2o/ci/phpbase/newviews/templates/
 
-domain2="o2o@112.126.72.144"
+# domain2="o2o@112.126.72.144"
 
-scp -r dist/static/*  ${domain2}:/home/o2o/webroot/static/
-scp -r dist/template/*  ${domain2}:/home/o2o/ci/phpbase/newviews/templates/
+# scp -r dist/static/*  ${domain2}:/home/o2o/webroot/static/
+# scp -r dist/template/*  ${domain2}:/home/o2o/ci/phpbase/newviews/templates/
 
-domain="root@115.28.146.162"
+# domain="root@115.28.146.162"
 
-scp -r dist/static/*  ${domain}:/home/o2o/static/
-scp -r dist/template/*  ${domain}:/home/o2o/phpbase/newviews/templates/
+# scp -r dist/static/*  ${domain}:/home/o2o/static/
+# scp -r dist/template/*  ${domain}:/home/o2o/phpbase/newviews/templates/
